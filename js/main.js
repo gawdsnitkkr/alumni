73cab4ad8df226ff98bffc68ab15778f29bd46c0
Anuj Sharma <anujjangra25119@gmail.com>
2020-04-04 18:04:49 +0530

    Revert "added profiles"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const MAX_TITLE_LENGTH = 35;
 const getProfile = profile => {
   const { name, imageUrl, profileLink, batch, title } = profile;
 
-  
   return `
     <div class="card">
     
@@ -31,19 +30,10 @@ const getProfile = profile => {
 
 const content = document.getElementById("content");
 
-function GetSortOrder(prop) {  
-  return function(a, b) {  
-      if (parseInt(a[prop]) > parseInt(b[prop])) {  
-          return 1;  
-      } else if (parseInt(a[prop]) < parseInt(b[prop])) {  
-          return -1;  
-      }  
-      return 0;  
-  }  
-} 
+const compare = (a, b) => parseInt(a.batch) > parseInt(b.batch);
 
 let contentHtml = "";
-profiles.data.sort(GetSortOrder("batch"));
+profiles.data.sort(compare);
 profiles.data.forEach(profile => {
   contentHtml += getProfile(profile);
 });
